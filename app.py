93e3fc6c3f4f8fc40f9f4d433502f45949def3a9
Yashrajdps13 <yashrajdps@gmail.com>
2024-08-25 12:21:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# app.py
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins="*")
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'Hello World'})
+
+@app.route('/getRoute', methods=['POST'])
+def index():
+    data = request.get_json()
+    print(data)
+    data_model = [[17.686815, 83.218483], [11.6234, 92.7265]]
+    return jsonify({"Path": data_model})
+
+if __name__ == '__main__':
+    app.run(debug=True)
